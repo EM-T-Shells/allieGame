c0a0dc06b16df7fc9ff902e1cb46e912f34ef7f8
levius_codeWell <taskaleg@gmail.com>
2023-12-20 11:17:30 -0500

    added png's

--- game.js
+++ game.js
@@ -1,63 +1,61 @@
-// Get the canvas and context
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
+ // Get the canvas and context
+ const canvas = document.getElementById('gameCanvas');
+ const ctx = canvas.getContext('2d');
 
-// Character variables
-let femaleX = 50;
-let femaleY = canvas.height - 50;
-let maleX = canvas.width - 100;
-let maleY = canvas.height - 50;
+ // Character variables
+ let femaleX = 50;
+ let femaleY = canvas.height - 200;
+ let maleX = canvas.width - 200;
+ let maleY = canvas.height - 200;
 
-// Heart position
-const heartX = canvas.width - 50;
-const heartY = canvas.height - 50;
+ // Flag to check if characters are hugging
+ let isHugging = false;
 
-// Flag to check if characters are hugging
-let isHugging = false;
+ // Load images
+ const femaleImage = new Image();
+ femaleImage.src = 'images/femaleChar.png';  // Replace with your image file
 
-// Listen for arrow key press
-document.addEventListener('keydown', moveFemale);
+ const maleImage = new Image();
+ maleImage.src = 'images/maleChar.png';  // Replace with your image file
 
-function moveFemale(event) {
-    if (event.key === 'ArrowRight' && !isHugging) {
-        femaleX += 10;
+ // Listen for arrow key press
+ document.addEventListener('keydown', moveFemale);
 
-        // Check if female character reaches the heart
-        if (femaleX > heartX) {
-            // Female character reaches the heart, male character appears
-            isHugging = true;
-        }
+ function moveFemale(event) {
+     if (event.key === 'ArrowRight' && !isHugging) {
+         femaleX += 10;
 
-        // Draw the characters
-        drawCharacters();
-    }
-}
+         // Check if female character reaches the male character
+         if (femaleX == maleX - 120) {
+             // Female character reaches the male character, hugging begins
+             isHugging = true;
+         }
 
-// Draw characters on the canvas
-function drawCharacters() {
-    // Clear the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+         // Draw the characters
+         drawCharacters();
+     }
+ }
 
-    // Draw female character
-    ctx.fillStyle = 'black';
-    ctx.fillRect(femaleX, femaleY, 20, 20);
+ // Draw characters on the canvas
+ function drawCharacters() {
+     // Clear the canvas
+     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw male character
-    if (isHugging) {
-        ctx.fillRect(maleX, maleY, 20, 20);
-    }
+     // Draw male character
+     if (isHugging) {
+         ctx.drawImage(maleImage, maleX, maleY, 150, 150);
+     }
 
-    // Draw heart
-    ctx.fillStyle = 'red';
-    ctx.fillRect(heartX, heartY, 20, 20);
+     // Draw female character
+     ctx.drawImage(femaleImage, femaleX, femaleY, 150, 150);
 
-    // If characters are hugging, display the message
-    if (isHugging) {
-        ctx.fillStyle = 'black';
-        ctx.font = '20px Arial';
-        ctx.fillText('I LOVE YOU. MERRY CHRISTMAS', canvas.width / 2 - 150, canvas.height / 2);
-    }
-}
+     // If characters are hugging, display the message
+     if (isHugging) {
+         ctx.fillStyle = 'black';
+         ctx.font = '20px Arial';
+         ctx.fillText('I LOVE YOU. MERRY CHRISTMAS', canvas.width / 2 - 150, canvas.height / 2);
+     }
+ }
 
-// Initial draw
-drawCharacters();
+ // Initial draw
+//  drawCharacters();
